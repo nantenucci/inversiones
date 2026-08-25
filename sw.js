bf17122a8d89d6085741484ca26b58d09130a1ab
nantenucci <nantenucci@weblegal.com.ar>
2026-08-25 13:19:14 -0300

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'inversiones-antenucci-v1';
+const ASSETS = ['/inversiones/', '/inversiones/index.html', '/inversiones/manifest.json', '/inversiones/inversiones-icon-192.png', '/inversiones/inversiones-icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Red primero: así una actualización de la app se ve de inmediato.
+// Si no hay conexión, sirve la última versión guardada en caché.
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copia = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copia));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
